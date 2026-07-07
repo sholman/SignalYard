bc04facc5a72d81a28511a207326a3c2b3d532af
Simon Holman <simon@simonholman.dev>
2026-07-07 14:46:25 +0930

    Changed filter to be property specific

--- src/SignalYard.Web/Program.cs
+++ src/SignalYard.Web/Program.cs
@@ -76,7 +76,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
+    app.UseExceptionHandler("/Error");
     app.UseHsts();
 }
 
